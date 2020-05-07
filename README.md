276ada85068043d59a1f7704dd708505712965aa
BLoveless <bloveless@talentpath.com>
2020-05-07 15:26:40 -0400

    ddd

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # java_maza_project
 Making a java program to run a java maze
+
+
+From edx course DEV276x: Learn to Program in Java!
+
+
+For your final project you will be writing the code for "MazeRunner", 
+a program that lets a user navigate through a given maze. 
+
+
+The code for the Maze is already written, and provided in "Maze.java". 
+
+
+You just need to write the code that uses Maze and decides how to move through it.
+
+
--- src/Maze/MazeMain.java
+++ src/Maze/MazeMain.java
@@ -3,6 +3,19 @@ import Maze.Maze;
 
 import java.util.*;
 
+/// use downloaded maze program
+/// show map
+/// code interface for player to move in map (up down left right)
+/// allow user to jump 'pits'
+/// keep track of moves ( 100 moves = dead if still in maze)
+
+
+/// intro method
+// Your map will look like all "." Characters and one "x" character.
+// The "x" represents your current position and the "." Represents an unknown space.
+// As you move through the map the "." Will turn into either walls ("-") or free spaces ("*").
+// There might even be more surprises in store for you as well.
+
 
 public class MazeMain {
 
